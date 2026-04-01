0d7648a34e6558823afedd574bd65692c2a8e49a
Jonathan Ellis <joellis13@yahoo.com>
2026-04-01 10:47:43 -0500

    initial commit

---
+++ src/main/java/com/jellisisland/docugap/DocugapApplication.java
@@ -0,0 +1,13 @@
+package com.jellisisland.docugap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocugapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocugapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jellisisland/docugap/DocugapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jellisisland.docugap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocugapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
