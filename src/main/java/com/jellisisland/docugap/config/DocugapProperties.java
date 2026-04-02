44ef35cda46dd7894f6048d0a20efc549e80aef2
Jonathan Ellis <joellis13@yahoo.com>
2026-04-01 22:18:53 -0500

    formatting

--- src/main/java/com/jellisisland/docugap/config/DocugapProperties.java
+++ src/main/java/com/jellisisland/docugap/config/DocugapProperties.java
@@ -1,6 +1,8 @@
 package com.jellisisland.docugap.config;
+
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+
 @Data
 @ConfigurationProperties(prefix = "docugap")
 public class DocugapProperties {
@@ -9,16 +11,19 @@ public class DocugapProperties {
     private OutputProperties output = new OutputProperties();
     private AtlassianProperties atlassian = new AtlassianProperties();
     private GitHubProperties github = new GitHubProperties();
+
     @Data
     public static class LlmProperties {
         private String provider = "anthropic";
     }
+
     @Data
     public static class PipelineProperties {
         private int maxEpicsPerRun;
         private int epicLookbackDays;
         private boolean pauseForApproval;
     }
+
     @Data
     public static class OutputProperties {
         private boolean console;
@@ -26,6 +31,7 @@ public class DocugapProperties {
         private String jsonPath;
         private boolean confluence;
     }
+
     @Data
     public static class AtlassianProperties {
         private String cloudId;
@@ -33,6 +39,7 @@ public class DocugapProperties {
         private String confluenceSpaceKey;
         private String sessionLogPageId;
     }
+
     @Data
     public static class GitHubProperties {
         private String defaultRepo;
